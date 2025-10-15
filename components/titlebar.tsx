6696d2e98c98c0938af4160dd94bc2b22ba2aaaa
Hasin Raiyan <fullcopyrightsounds@gmail.com>
2025-10-15 12:16:32 +0600

    push

--- components/titlebar.tsx
+++ components/titlebar.tsx
@@ -4,7 +4,7 @@ export function Titlebar() {
   return (
     <div className="titlebar">
       <div className="flex items-center gap-2 px-3">
-        <img src="/icons/app-icon.png" alt="Sweesh" width={16} height={16} />
+        <img src="/icons/32x32.png" alt="Sweesh" width={16} height={16} />
         <span className="text-sm font-medium text-foreground">Sweesh</span>
       </div>
     </div>
