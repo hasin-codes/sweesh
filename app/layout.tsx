e61305d57cbd694e721846c2123e64fbb18d6596
Hasin Raiyan <fullcopyrightsounds@gmail.com>
2025-10-15 10:27:44 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,9 +68,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body
         className={`${editorsNote.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         <Titlebar />
         {children}
